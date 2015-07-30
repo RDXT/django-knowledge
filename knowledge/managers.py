08c13d4427da8a3687b024eb99f16ed6e3bea464
Scott Adams <scottadams80@gmail.com>
2015-07-30 23:12:37 +0200

    Fixes get_query_set

--- knowledge/managers.py
+++ knowledge/managers.py
@@ -3,12 +3,11 @@ from django.db.models import Q
 
 
 class QuestionManager(models.Manager):
-    # def get_query_set(self, *args, **kwargs):
-    #     return super(QuestionManager, self).get_query_set(*args, **kwargs)
-
     def can_view(self, user):
-        qs = super(QuestionManager, self).get_query_set()\
-                                         .select_related('user')
+        method = getattr(
+            super(QuestionManager, self), 'get_queryset',
+            getattr(super(QuestionManager, self), 'queryset', None))
+        qs = method().select_related('user')
 
         if user.is_staff or user.is_superuser:
             return qs.all()
@@ -23,37 +22,31 @@ class QuestionManager(models.Manager):
 
 class ResponseManager(models.Manager):
     # def all(self, *args, **kwargs):
-    #     return super(ResponseManager, self).all(*args, **kwargs)\
-    #                                        .select_related('question', 'user')
+    # return super(ResponseManager, self).all(*args, **kwargs)\
+    # .select_related('question', 'user')
 
     def can_view(self, user):
-        qs = super(ResponseManager, self).get_query_set()\
-                                         .select_related('question', 'user')
+        method = getattr(
+            super(ResponseManager, self), 'get_queryset',
+            getattr(super(ResponseManager, self), 'queryset', None))
+        qs = method().select_related('question', 'user')
 
         if user.is_staff or user.is_superuser:
             return qs.all()
 
         if user.is_anonymous():
             return qs.filter(
-                Q(status='public') |
-                Q(status='inherit', question__status='public')
+                Q(status='public') | Q(status='inherit', question__status='public')
             )
 
         # ooooh boy this is crazy!
         return qs.filter(
-            Q(status='public') |
-            Q(  # respect private parent user/status
-                Q(status='private') &
-                Q(
-                    Q(user=user) |
-                    Q(question__user=user)
-                )
-            ) |
-            Q(  # follow inherited status/users
-                Q(status='inherit') &
-                Q(
-                    Q(question__status='public') |
-                    Q(question__status='private', question__user=user)
-                )
-            )
+            Q(status='public') | Q(  # respect private parent user/status
+                                     Q(status='private') & Q(
+                                         Q(user=user) | Q(question__user=user)
+                                     )) | Q(  # follow inherited status/users
+                                              Q(status='inherit') & Q(
+                                                  Q(question__status='public') | Q(question__status='private',
+                                                                                   question__user=user)
+                                              ))
         )
